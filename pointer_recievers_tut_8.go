934fa9c3a2b5a41edc1327d892674cac86a7f986
Siddharth Kale <siddharthkale97@gmail.com>
2020-07-10 00:31:50 +0530

    Pointer Recievers

--- pointer_recievers_tut_8.go
+++ pointer_recievers_tut_8.go
@@ -22,6 +22,17 @@ func (c car) kmh() float64 {
 	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax)
 }
 
+// Value reciever makes a copy
+func (c *car) new_top_speed(newspeed float64) {
+	c.top_speed_kmh = newspeed
+}
+
+// Just a function instead of a method
+func newer_top_speed(c car, speed float64) car {
+	c.top_speed_kmh = speed
+	return c
+}
+
 func main() {
 	a_car := car{gas_pedal: 22341, // can use semicolon as we are inside a function
 		brake_pedal:    0,
@@ -29,5 +40,9 @@ func main() {
 		top_speed_kmh:  225.0}
 	// b_car := car{21341, 0, 12561, 225.0} // shorten format
 	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.kmh())
+	a_car.new_top_speed(500)
+	fmt.Println(a_car.kmh())
+	a_car.new_top_speed(400)
 	fmt.Println(a_car.kmh())
 }

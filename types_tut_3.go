53b65c15cdf3aad526931b72d58ea55267512cd7
Siddharth Kale <siddharthkale97@gmail.com>
2020-07-09 22:00:02 +0530

    data-types

--- types_tut_3.go
+++ types_tut_3.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func add(x float64, y float64) float64 {
+func add(x, y float64) float64 {
 	return x + y
 }
 
 func main() {
-	var num1 float64 = 5.6
-	var num2 float64 = 9.5
+	var num1, num2 float64 = 5.6, 9.5
+	// var num2 float64 = 9.5
 	fmt.Println(add(num1, num2))
 }

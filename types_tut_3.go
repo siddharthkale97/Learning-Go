b9adcc7750861b5fac5fda0087b479d1cf6dc6f0
Siddharth Kale <siddharthkale97@gmail.com>
2020-07-09 23:11:26 +0530

    Types fin

--- types_tut_3.go
+++ types_tut_3.go
@@ -6,8 +6,22 @@ func add(x, y float64) float64 {
 	return x + y
 }
 
+func multiple(a, b string) (string, string) { // if return multiple things
+	// specific every return type
+	return a, b
+}
+
 func main() {
-	var num1, num2 float64 = 5.6, 9.5
+	// var num1, num2 float64 = 5.6, 9.5 // num1 will have a fixed type if complied
+	num1, num2 := 5.6, 9.5 //short hand,
+	//will get a default type automatically when complied
 	// var num2 float64 = 9.5
 	fmt.Println(add(num1, num2))
+	// if variables and imports are not used, then error are thrown
+
+	w1, w2 := "Word 1", "Word 2"
+	fmt.Println(multiple(w1, w2))
+	var a int = 62
+	var b float64 = float64(a)
+	fmt.Print(b)
 }
